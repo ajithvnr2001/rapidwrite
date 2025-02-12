4df4f3dca8a98337f37952d937a2a487eae67754
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2025-02-12 22:49:41 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
-from pydantic import Field
+from pydantic import Field, HttpUrl
 
 
 load_dotenv()
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     glpi_user_token: str = "hN04d5lgFrkmprGz183sPfUhXt04102DF10pUFsN"
     meilisearch_url: str = "https://ms-6c0bfee744cb-18789.nyc.meilisearch.io"
     meilisearch_master_key: str = "289e33eaa93280a3955fbed03a73fce65048d51c"
-    wasabi_endpoint: str = "s3.ap-northeast-1.wasabisys.com"
+    wasabi_endpoint: HttpUrl = "https://s3.ap-northeast-1.wasabisys.com"
     wasabi_access_key: str = "WFDR91CL1S6EII3LFKHV"
     wasabi_secret_key: str = "rGnTDbiXOm3IFpRiaAZm9jGkfXJTGpylrJbuZ0et"
     openai_api_base: str = "https://chatapi.akash.network/api/v1"
